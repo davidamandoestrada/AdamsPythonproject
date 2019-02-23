9f65a79e3e4fbf31e35877f16cacb05df2107a5b
David Estrada <davidamandoestrada@gmail.com>
2019-02-23 14:07:12 -0800

    Added a Frames class that will encapsulate a lot of our logic for manipulating frames

---
+++ frames.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+
+
+class Frames:
+    def __init__(self):
+        self._frames = {}
+        self._ids = []
+        self._current_id_index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._current_id_index >= len(self._ids):
+            raise StopIteration
+        else:
+            self._current_id_index += 1
+            return self._ids[self._current_id_index - 1]
+
+    def add(self, read):
+        if read.id not in self.frames:
+            self.frames[read.id] = {"Time": [], "Voltage": [], "KWH": []}
+            self._ids.append(read.id)
+        self.frames[read.id]["Time"].append(read.time)
+        self.frames[read.id]["Voltage"].append(read.voltage)
+        self.frames[read.id]["KWH"].append(read.kwh)
+
+    @property
+    def frames(self):
+        return self._frames
+
+    def get_frame_by_id(self, id: str):
+        if id in self.frames:
+            return self.frames[id]
+        else:
+            raise LookupError
+
+    def get_time_sorted_frame_by_id(self, id: str):
+        frame = self.get_frame_by_id(id)
+        return self._sort_voltage_and_kwh_by_time(frame)
+
+    def _sort_voltage_and_kwh_by_time(self, dictionary_of_time_voltage_kwh):
+        sorted_dictionary = {}
+        sorted_dictionary["Time"], sorted_dictionary["Voltage"], sorted_dictionary["KWH"] = zip(
+            *sorted(zip(dictionary_of_time_voltage_kwh["Time"], dictionary_of_time_voltage_kwh["Voltage"], dictionary_of_time_voltage_kwh["KWH"])))
+        return sorted_dictionary
--- main.py
+++ main.py
@@ -6,22 +6,18 @@ from pandas import DataFrame, read_csv
 
 from config import FAKE_VOLTAGE_DATA_PATH
 from electric_read import ElectricRead
-from utility import sort_voltage_and_kwh_by_time
+from frames import Frames
 
 rows = read_csv(FAKE_VOLTAGE_DATA_PATH)
 electric_reads = [ElectricRead(time=row["Time"], id=row["ID"],
                                voltage=row["Voltage"], kwh=row["KWH"]) for index, row in rows.iterrows()]
 
-frames = {}
+frames = Frames()
 for read in electric_reads:
-    if read.id not in frames:
-        frames[read.id] = {"Time": [], "Voltage": [], "KWH": []}
-    frames[read.id]["Time"].append(read.time)
-    frames[read.id]["Voltage"].append(read.voltage)
-    frames[read.id]["KWH"].append(read.kwh)
+    frames.add(read)
 
 for id in frames:
-    df = DataFrame(data=sort_voltage_and_kwh_by_time(frames.get(id)))
+    df = DataFrame(data=frames.get_time_sorted_frame_by_id(id))
     logging.info(f"New dataframe for ID {id}")
     pyplot.plot(df["Time"], df["Voltage"])
 
