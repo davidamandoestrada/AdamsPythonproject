260f96fb8b33e87204c946f671d5a46c979be9dd
David Estrada <davidamandoestrada@gmail.com>
2019-02-23 16:23:28 -0800

    Added some keyword parameters in the invocations in main.py, to make it more clear what is happening. Fixed some spacing issues

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from electric_read import ElectricRead
 from frames import Frames
 
 electric_reads = [ElectricRead(time=row["Time"], id=row["ID"],
-                               voltage=row["Voltage"], kwh=row["KWH"]) for index, row in read_csv(FAKE_VOLTAGE_DATA_PATH).iterrows()]
-frames = Frames(electric_reads)
+                               voltage=row["Voltage"], kwh=row["KWH"]) for index, row
+                  in read_csv(filepath_or_buffer=FAKE_VOLTAGE_DATA_PATH).iterrows()]
+frames = Frames(reads=electric_reads)
 
 for id in frames:
-    frames.add_frame_to_plot_by_id(id)
+    frames.add_frame_to_plot_by_id(id=id)
 
 frames.show_plots()
